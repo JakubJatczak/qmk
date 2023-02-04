41204750066d46c4c57eb9264d7ae6322ec1baaf
Jakub Jatczak <jjatczak@antmicro.com>
2023-02-04 22:42:10 +0100

    cantor: Add my layout

---
+++ keyboards/cantor/keymaps/minimalColemakDHm/config.h
@@ -0,0 +1,9 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef TAPPING_TERM
+#define TAPPING_TERM 190
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
