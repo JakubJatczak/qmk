6c5d52f5f0bd4eb5f99ad43a50a209e50017dfdd
Jakub Jatczak <jjatczak@antmicro.com>
2023-02-10 12:40:31 +0100

    Set right half as master

--- keyboards/cantor/keymaps/minimalColemakDHm/config.h
+++ keyboards/cantor/keymaps/minimalColemakDHm/config.h
@@ -2,6 +2,7 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#define MASTER_RIGHT
 #define IGNORE_MOD_TAP_INTERRUPT
 #undef TAPPING_TERM
 #define TAPPING_TERM 190
