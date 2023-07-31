237d56b5ad545a98c39fa46c42f572e9062f6bda
Jakub Jatczak <jjatczak@antmicro.com>
2023-07-31 22:16:42 +0200

    Improve tap/hold experience

--- keyboards/cantor/keymaps/minimalColemakDHm/config.h
+++ keyboards/cantor/keymaps/minimalColemakDHm/config.h
@@ -3,9 +3,11 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define MASTER_RIGHT
-#define IGNORE_MOD_TAP_INTERRUPT
+
 #undef TAPPING_TERM
 #define TAPPING_TERM 190
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 
 #define COMBO_COUNT 4
 
